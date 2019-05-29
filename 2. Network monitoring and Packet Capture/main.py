b2cfa5885ba1480b1dd4d86ac09ed01805badc60
hardhat <hardhatgamingmachine@gmail.com>
2019-05-29 14:21:47 +0530

    Added code for packet capture

--- 2. Network monitoring and Packet Capture/main.py
+++ 2. Network monitoring and Packet Capture/main.py
@@ -1,5 +1,34 @@
-import pyshark 
+import pyshark
+import re
 
-capture = pyshark.LiveCapture(interface='mon1')
-capture.sniff(timeout=10)
-capture
+match = 'Type/Subtype: Probe Request (0x0004)'
+macs = '5c:99:60:bf:44:43'
+
+def get_signal_strength(packet):
+    layer = packet[1]
+    text = str(layer)
+    arr = text.splitlines()
+    ss = arr[6]
+    a = [int(s) for s in re.findall(r'\d+', ss)]
+    return a[0]
+
+def get_mac_address(packet):
+    layer = packet[2]
+    text = str(layer)
+    arr = text.splitlines()
+    mac = arr[17]
+    mac = mac.strip()
+    mac = mac.split("address: ",1)[1] 
+    return mac
+
+cap = pyshark.LiveCapture(interface='mon0', display_filter='wlan.fc.type_subtype==4')
+
+for packet in cap.sniff_continuously():
+    #packet_type = get_packet_type(packet)
+    #if(packet_type==match):
+    try:
+        mac = get_mac_address(packet)
+        ss = get_signal_strength(packet)
+        print(mac, ss)
+    except:
+        pass
